174cbb6099507d0aef4bd665143ab2c61f8386fc
ryowuandjanet <ryowudata@gmail.com>
2020-10-08 10:35:43 +0800

    commit

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyBNGysEd46Bb6SOzWEV_v_fMPllC1UafMY",
+  authDomain: "test-eeb83.firebaseapp.com",
+  databaseURL: "https://test-eeb83.firebaseio.com",
+  projectId: "test-eeb83",
+  storageBucket: "test-eeb83.appspot.com",
+  messagingSenderId: "170996932361",
+  appId: "1:170996932361:web:ca0c732fb1e23a14d37dd8"
+}
